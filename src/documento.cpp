293a1e8cbc5498bfc653af3ab2d4becb4b69397d
Antônio Isaac Silva Lima <isaac.lima1@outlook.com>
2019-11-22 11:20:26 +0000

    Tudo funfando, vou terminar os testes agora

--- src/documento.cpp
+++ src/documento.cpp
@@ -3,30 +3,34 @@
 #include <cmath>
 #include <map>
 
-using std::string; using std::multimap;
-using std::vector; using std::pair;
+using std::string; 		using std::multimap;
+using std::vector; 		using std::pair;
 using std::cout;
 
-Documento::Documento(){
-}
+Documento::Documento()
+{}
 
 Documento::Documento(std::string id){
     docid_ = id;
 }
 
-void Documento::Mudar_id(std::string id){
+void Documento::Mudar_id(std::string id)
+{
     docid_ = id;
 }
 
-std::string Documento::Get_id(){
+std::string Documento::Get_id()
+{
     return docid_;
 }
 
-void Documento::Inserir_coordenada(double coord){
+void Documento::Inserir_coordenada(double coord)
+{
     coordenadas_.push_back(coord);
 }
 
-double Documento::operator[](int indice){
+double Documento::operator[](int indice)
+{
     return coordenadas_[indice];
 }
 
@@ -69,7 +73,8 @@ void Documento::Obter_coordenadas(Indice_invertido& Indice)
 	Imprimir_coordenadas();
 }
 
-void Documento::Imprimir_coordenadas(){
+void Documento::Imprimir_coordenadas()
+{
     cout << docid_ << " = {";
     for (int i = 0; i < coordenadas_.size() - 1; i++)
         cout << coordenadas_[i] << ", ";
@@ -77,20 +82,22 @@ void Documento::Imprimir_coordenadas(){
     cout << coordenadas_[coordenadas_.size()-1] << "}\n";
 }
 
-double Documento::Similaridade(Documento& query){
-    double somatorio_produto,somatorio_quadrado1,somatorio_quadrado2,cos;
+double Documento::Similaridade(Documento& query)
+{
+    long double somatorio_produto,somatorio_quadrado1,somatorio_quadrado2,cos;
     
-    somatorio_produto = 0;
-    for(int i = 0;i < coordenadas_.size();i++)
+    somatorio_produto = 0.0;
+    for(int i = 0; i < coordenadas_.size(); i++)
         somatorio_produto += coordenadas_[i]*(query.coordenadas_[i]);
     
-    somatorio_quadrado1 = 0;
-    for(int i = 0;i < coordenadas_.size();i++)
+    somatorio_quadrado1 = 0.0;
+    for(int i = 0; i < coordenadas_.size(); i++)
         somatorio_quadrado1 += (coordenadas_[i]*coordenadas_[i]);
     
-    for(int i = 0;i < query.coordenadas_.size();i++)
-        somatorio_quadrado2 += (query.coordenadas_[i])*(query.coordenadas_[i]);
-    
+	somatorio_quadrado2 = 0.0;
+    for(int i = 0; i < query.coordenadas_.size(); i++)
+		somatorio_quadrado2 += (query.coordenadas_[i])*(query.coordenadas_[i]);
+
     cos = (somatorio_produto)/(sqrt(somatorio_quadrado1)*sqrt(somatorio_quadrado2));
     return cos;
 }
